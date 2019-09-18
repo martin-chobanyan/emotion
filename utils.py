3435b0e21e9f5dfa39d95d6a6264a243afebd78c
Martin Chobanyan <chobanyan.martin@gmail.com>
2019-09-18 01:45:55 -0400

    refactor and documentation for utils.py

--- utils.py
+++ utils.py
@@ -7,43 +7,59 @@ import numpy as np
 from tqdm import tqdm
 from PIL import Image
 
-import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
-from sklearn.utils.multiclass import unique_labels
 
+class RecoverImage:
+    """Recover the original image after the ToTensor and Normalize transformations have been applied
 
-# TODO: move this to data science kit
-def plot_confusion_matrix(y_true, y_pred, classes, title=None, cmap=plt.cm.Blues, figsize=(6, 4)):
-    if not title:
-        title = 'Confusion matrix'
-    cm = confusion_matrix(y_true, y_pred)
-    classes = classes[unique_labels(y_true, y_pred)]
+    Parameters
+    ----------
+    means: list[float], optional
+        A list of per channel means. If None, then it is assumed that Normalize was not used (default=None).
+    stdvs: list[float], optional
+        A list of per channel standard deviations.
+        If None, then it is assumed that Normalize was not used (default=None).
+    """
 
-    fig, ax = plt.subplots(figsize=figsize)
-    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
-    ax.figure.colorbar(im, ax=ax)
-    ax.set(xticks=np.arange(cm.shape[1]),
-           yticks=np.arange(cm.shape[0]),
-           xticklabels=classes, yticklabels=classes,
-           title=title,
-           ylabel='True label',
-           xlabel='Predicted label')
-    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-             rotation_mode="anchor")
+    def __init__(self, means=None, stdvs=None):
+        self.means = means
+        self.stdvs = stdvs
 
-    fmt = 'd'
-    thresh = cm.max() / 2.
-    for i in range(cm.shape[0]):
-        for j in range(cm.shape[1]):
-            ax.text(j, i, format(cm[i, j], fmt),
-                    ha="center", va="center",
-                    color="white" if cm[i, j] > thresh else "black")
-    fig.tight_layout()
-    return ax
+    def __call__(self, x):
+        """Recover the image
+
+        Parameters
+        ----------
+        x: torch.Tensor
+            The transformed image tensor
+
+        Returns
+        -------
+        PIL.Image.Image
+            The recovered image as a PIL Image
+        """
+        if self.means is not None:
+            means = torch.Tensor(self.means).view(3, 1, 1)
+            stdvs = torch.Tensor(self.stdvs).view(3, 1, 1)
+            x = (x * stdvs) + means
+        x *= 255
+        x = x.permute(1, 2, 0).numpy().astype(np.uint8)
+        return Image.fromarray(x)
 
 
 def calc_norm_stats(data, num_channels=3):
-    """Dataset must return tensors with shape [num_channels, height, width]"""
+    """Estimates the per channel means and standard deviations for a Dataset
+
+    Parameters
+    ----------
+    data: Dataset
+    num_channels: int, optional
+        The number of channels in each image (default=3)
+
+    Returns
+    -------
+    list[float], list[float]
+        A list of per channel means and a list of per channel standard deviations
+    """
     n = len(data)
     means = np.zeros(num_channels)
     stdvs = np.zeros(num_channels)
@@ -54,18 +70,3 @@ def calc_norm_stats(data, num_channels=3):
     means /= n
     stdvs /= n
     return means.tolist(), stdvs.tolist()
-
-
-class RecoverImage:
-    def __init__(self, means=None, stdvs=None):
-        self.means = means
-        self.stdvs = stdvs
-
-    def __call__(self, x):
-        if self.means is not None:
-            means = torch.Tensor(self.means).view(3, 1, 1)
-            stdvs = torch.Tensor(self.stdvs).view(3, 1, 1)
-            x = (x * stdvs) + means
-        x *= 255
-        x = x.permute(1, 2, 0).numpy().astype(np.uint8)
-        return Image.fromarray(x)
