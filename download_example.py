a2215cce0a13cb021a24dac0826b1760a5b4bb72
mcGIT123 <chobanyan.martin@gmail.com>
2019-10-26 19:17:34 -0400

    made chrome driver variable

--- download_example.py
+++ download_example.py
@@ -3,12 +3,13 @@
 
 from google_images_download import google_images_download
 
+DRIVER_PATH = 'insert the filepath to the chrome driver here...'
 args = {'keywords': 'happy human face',
         'output_directory': '/data/emotion/images/',
         'image_directory': 'happy',
         'silent_mode': True,
         'limit': 1000,
-        'chromedriver': 'path to chrome driver'}
+        'chromedriver': DRIVER_PATH}
        
 response = google_images_download.googleimagesdownload()
 response.download(args)
